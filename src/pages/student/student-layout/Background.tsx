f65574decfd1ebbcef3e1ea77f3513707c4d5d78
Grzesiek <dadmor@gmail.com>
2025-08-11 18:19:19 +0200

    0.1.29

--- src/pages/student/student-layout/Background.tsx
+++ src/pages/student/student-layout/Background.tsx
@@ -8,28 +8,15 @@ export const Background: React.FC = () => {
           <div className="absolute inset-0 bg-background rounded-3xl" />
           <div
             className="absolute inset-0 rounded-3xl overflow-hidden"
-            style={{
-              WebkitMaskImage:
-                "linear-gradient(to bottom, transparent 0%, transparent 50%, black 70%)",
-              maskImage:
-                "linear-gradient(to bottom, transparent 0%, transparent 50%, black 70%)",
-            }}
           >
-            {/* Main gradient for base tone */}
-            <div className="absolute inset-0 bg-gradient-to-r from-orange/10 via-yellow/5 to-purple/10 rounded-3xl" />
+            {/* Left large orange blob - positioned at bottom left */}
+            <div className="absolute w-[50vw] h-[50vh] bottom-0 left-0 translate-y-[25%] -translate-x-[25%] bg-orange rounded-full blur-[150px] opacity-70 dark:opacity-40" />
             
-            {/* Large orange blob on the left - creating concave effect */}
-            <div className="absolute w-[90vw] h-[120vh] top-1/2 -translate-y-1/2 -left-[40%] bg-orange rounded-full blur-[180px] opacity-70 dark:opacity-40 animate-pulse" />
+            {/* Center smaller yellow blob - positioned at bottom center */}
+            <div className="absolute w-[30vw] h-[30vh] bottom-0 left-1/2 -translate-x-1/2 translate-y-[20%] bg-yellow rounded-full blur-[100px] opacity-60 dark:opacity-30" />
             
-            {/* Large purple blob on the right - creating concave effect */}
-            <div className="absolute w-[90vw] h-[120vh] top-1/2 -translate-y-1/2 -right-[40%] bg-purple rounded-full blur-[180px] opacity-70 dark:opacity-40 animate-pulse" />
-            
-            {/* Central yellow glow - smaller and in the middle */}
-            <div className="absolute w-[40vw] h-[40vw] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-yellow rounded-full blur-[120px] opacity-60 dark:opacity-30 animate-pulse" />
-            
-            {/* Additional depth layers */}
-            <div className="absolute w-[60vw] h-[80vh] top-1/2 -translate-y-1/2 -left-[20%] bg-orange rounded-full blur-[200px] opacity-40 dark:opacity-20" />
-            <div className="absolute w-[60vw] h-[80vh] top-1/2 -translate-y-1/2 -right-[20%] bg-purple rounded-full blur-[200px] opacity-40 dark:opacity-20" />
+            {/* Right large purple blob - positioned at bottom right */}
+            <div className="absolute w-[50vw] h-[50vh] bottom-0 right-0 translate-y-[25%] translate-x-[25%] bg-purple rounded-full blur-[150px] opacity-70 dark:opacity-40" />
           </div>
         </div>
       </div>
